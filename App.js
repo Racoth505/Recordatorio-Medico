63ab781b812a0c40584386a6e613ba35eec8aa70
Racoth505 <rafafl2001@gmail.com>
2025-11-06 19:22:25 -0600

    Pruebas

--- App.js
+++ App.js
@@ -5,10 +5,13 @@ const icon = require('./assets/icon.png');
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={icon} style={{
-        width: 500, 
-        height: 500,
+
+      <Image source={{uri: "https://wiki.leagueoflegends.com/en-us/images/Chibi_Gwen_Base_Tier_1.png?fc9c7"}} 
+        style={{
+        width: 512, 
+        height: 512,
         resizeMode: 'center'}}/>
+
       <Text>Inicio de la aplicacion</Text>
       <StatusBar style="auto" />
     </View>
